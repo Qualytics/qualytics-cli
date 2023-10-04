276c795e5c89c50cb5af2ba8e030f1e579472e90
Rodrigo Ogawa <rodrigo@qualytics.co>
2023-10-04 00:41:27 -0300

    Fixed sync logic

--- qualytics/qualytics.py
+++ qualytics/qualytics.py
@@ -1,11 +1,11 @@
+import time
+
 import typer
 import os
 import json
 import requests
 import urllib3
 import re
-import click
-
 
 from pathlib import Path
 from urllib.parse import urlparse
@@ -14,7 +14,6 @@ from rich.progress import track
 from itertools import product
 from typing import Optional
 from typing_extensions import Annotated
-
 
 __version__ = "0.1.1"
 
@@ -53,7 +52,7 @@ def _get_default_headers(token):
         "Authorization": f"Bearer {token}"
     }
 
-        
+
 def distinct_file_content(file_path):
     with open(file_path, 'r') as file:
         # Using a set to automatically get distinct lines
@@ -67,7 +66,7 @@ def distinct_file_content(file_path):
 def log_error(message, file_path):
     with open(file_path, 'a') as file:
         file.write(message + '\n')
-        file.flush() 
+        file.flush()
 
 
 def get_quality_checks(base_url: str, token: str, datastore_id: int, output: str):
@@ -81,10 +80,10 @@ def get_quality_checks(base_url: str, token: str, datastore_id: int, output: str
     response = requests.get(url, headers=_get_default_headers(token), params=params, verify=False)
 
     data = response.json()
-    
+
     total = data["total"]
     all_quality_checks = []
-    
+
     total_pages = -(-total // size)
 
     # Loop through the pages based on total number and size
@@ -98,22 +97,54 @@ def get_quality_checks(base_url: str, token: str, datastore_id: int, output: str
         params["page"] = page
         response = requests.get(url, headers=_get_default_headers(token), params=params, verify=False)
         data = response.json()
-        
+
     print(f"[bold green] Total of Quality Checks = {data['total']} [/bold green]")
     print(f"[bold green] Total pages = {total_pages} [/bold green]")
     return all_quality_checks
 
 
-def get_table_ids(base_url: str, token: str, datastore_id: int):
-    response = requests.get(base_url + f"containers/listing?datastore={datastore_id}",
-                        headers=_get_default_headers(token), verify=False)
-    
-    items_array = response.json()
-    table_ids = {}
-    for item in items_array:
-        table_ids[item["name"]] = item["id"]
-    
-    return table_ids
+def get_quality_check_by_description(base_url: str, token: str, description: str):
+    endpoint = "quality-checks"
+    params = {
+        "search": description
+    }
+    url = f"{base_url}{endpoint}"
+
+    response = requests.get(url, headers=_get_default_headers(token), params=params, verify=False)
+
+    if response.status_code == 200:
+
+        quality_check = response.json()['items']
+
+        if len(quality_check) == 1:
+            return response.json()['items'][0]['id']
+
+    return None
+
+
+def get_table_ids(base_url: str, token: str, datastore_id: int, max_retries=5, retry_delay=5):
+    for attempt in range(max_retries):
+        try:
+            response = requests.get(base_url + f"containers/listing?datastore={datastore_id}",
+                                    headers=_get_default_headers(token), verify=False)
+
+            if response.status_code == 200:
+                items_array = response.json()
+                table_ids = {}
+                for item in items_array:
+                    table_ids[item["name"]] = item["id"]
+
+                return table_ids
+            else:
+                print(f"Attempt {attempt + 1} failed with status code {response.status_code}. Retrying...")
+                if attempt < max_retries - 1:  # Only sleep if it's not the last attempt
+                    time.sleep(retry_delay)  # Wait for a specified delay before retrying
+        except requests.RequestException as e:
+            print(f"Request error during attempt {attempt + 1}: {e}. Retrying...")
+            if attempt < max_retries - 1:  # Only sleep if it's not the last attempt
+                time.sleep(retry_delay)  # Wait for a specified delay before retrying
+    print(f"Failed getting the table ids after {max_retries} attempts.")
+    return None
 
 
 @app.callback(invoke_without_command=True)
@@ -138,7 +169,7 @@ def show_config():
 
 
 @app.command()
-def init(url: str = typer.Option(..., help="The URL to be set. Example: https://your-qualytics.qualytics.io/"), 
+def init(url: str = typer.Option(..., help="The URL to be set. Example: https://your-qualytics.qualytics.io/"),
          token: str = typer.Option(..., help="The token to be set.")):
     """
     Initialize the configuration.
@@ -146,31 +177,32 @@ def init(url: str = typer.Option(..., help="The URL to be set. Example: https://
     # Check if the URL starts with 'https://'
     if not url.startswith('https://'):
         url = 'https://' + url
-    
+
     # Check if the URL ends with '/'
     if not url.endswith('/') or not url.endswith('/api'):
         url += '/api/'
 
     # Check if the URL contains 'your-qualytics.qualytics.io'
     parsed_url = urlparse(url)
-    
+
     if not parsed_url.netloc.endswith('qualytics.io'):
         print("Invalid domain provided. The URL should be in the format: 'https://your-subdomain.qualytics.io/'")
         raise typer.Exit(code=1)
-    
+
     config = {
         "url": url,
         "token": token
     }
     save_config(config)
     print("Configuration saved!")
-    
+
 
 @checks_app.command("export")
-def checks_export(datastore: int = typer.Option(..., "--datastore", help="Datastore ID"), output: str = typer.Option(BASE_PATH + "/data_checks.json", "--output", help="Output file path")):
+def checks_export(datastore: int = typer.Option(..., "--datastore", help="Datastore ID"),
+                  output: str = typer.Option(BASE_PATH + "/data_checks.json", "--output", help="Output file path")):
     """
     Export checks to a file.
-    """    
+    """
     config = load_config()
     base_url = config['url']
     token = config['token']
@@ -179,10 +211,12 @@ def checks_export(datastore: int = typer.Option(..., "--datastore", help="Datast
     with open(output, 'w') as f:
         json.dump(all_quality_checks, f, indent=4)
     print(f"Data exported to {output}")
-    
-    
+
+
 @checks_app.command("import")
-def checks_import(datastore: str = typer.Option(..., "--datastore", help="Comma-separated list of Datastore IDs or array-like format"), input_file: str = typer.Option(BASE_PATH + "/data_checks.json", "--input", help="Input file path")):
+def checks_import(datastore: str = typer.Option(..., "--datastore",
+                                                help="Comma-separated list of Datastore IDs or array-like format"),
+                  input_file: str = typer.Option(BASE_PATH + "/data_checks.json", "--input", help="Input file path")):
     """
     Import checks from a file.
     """
@@ -191,65 +225,102 @@ def checks_import(datastore: str = typer.Option(..., "--datastore", help="Comma-
     config = load_config()
     base_url = config['url']
     token = config['token']
-    
+
     with open(input_file, 'r') as f:
         all_quality_checks = json.load(f)
         total_created_checks = 0
         total_updated_checks = 0
-        
+
         # Create pairs of datastore and quality_check to process
         pairs_to_process = list(product(datastores, all_quality_checks))
-        
+
         # Now use the track function to show the progress bar
         for datastore_id, quality_check in track(pairs_to_process, description="Processing..."):
             # for datastore_id in datastores:
-                table_ids = get_table_ids(base_url=base_url, token=token, datastore_id=datastore_id)
-                
-                # for quality_check in all_quality_checks:
-                container_id = None
+            table_ids = get_table_ids(base_url=base_url, token=token, datastore_id=datastore_id)
+
+            # for quality_check in all_quality_checks:
+            container_id = None
+            if table_ids:
                 try:
                     container_id = table_ids[quality_check['container']['name']]
                 except:
-                    print(f"[bold red] Profile `{quality_check['container']['name']}` was not found in datastore id: {datastore_id}[/bold red]")
-                    log_error(f"Profile `{quality_check['container']['name']}` of quality check #{quality_check['id']} was not found in datastore id: {datastore_id}", BASE_PATH + "/errors.log")
-                
+                    print(
+                        f"[bold red] Profile `{quality_check['container']['name']}` was not found in datastore id: {datastore_id}[/bold red]")
+                    log_error(
+                        f"Profile `{quality_check['container']['name']}` of quality check {quality_check['id']} was not found in datastore id: {datastore_id}",
+                        BASE_PATH + "/errors.log")
+
                 if container_id:
+                    description = f"[from quality check id: {quality_check['id']} - main datastore id: {datastore_id}]"
                     payload = {
                         "fields": [field['name'] for field in quality_check['fields']],
-                        "description": f"{quality_check['description']} [from quality check id: {quality_check['id']} - main datastore id: {datastore_id}]",
+                        "description": f"{quality_check['description']} {description}",
                         "rule": quality_check['rule_type'],
                         "coverage": quality_check['coverage'],
                         "is_new": quality_check['is_new'],
                         "filter": quality_check['filter'],
                         "properties": quality_check['properties'],
-                        "tags": [ global_tag['name'] for global_tag in quality_check['global_tags'] ],
+                        "tags": [global_tag['name'] for global_tag in quality_check['global_tags']],
                         "container_id": container_id
                     }
-                    response = requests.post(base_url + f"quality-checks", headers=_get_default_headers(token), json=payload, verify=False)
-                    if response.status_code == 409:
-                        print(f"[bold yellow]Quality check for container: {quality_check['container']['name']} was already created on datastore id: {datastore_id}. Updating check...[/bold yellow]")
-                        match = re.search(r"id: (\d+)", response.text)
-                        response = requests.put(base_url + f"quality-checks/{match.group(1)}", headers=_get_default_headers(token), json=payload, verify=False)
+                    # gets the quality_check by the description
+                    quality_check_id = get_quality_check_by_description(base_url=base_url, token=token,
+                                                                        description=description)
+
+                    # If a quality check contains the description, we sync
+                    if quality_check_id:
+                        print(
+                            f"[bold yellow]Quality check for container: {quality_check['container']['name']} was already created on datastore id: {datastore_id}. Updating quality check id: {quality_check_id}[/bold yellow]")
+                        response = requests.put(base_url + f"quality-checks/{quality_check_id}",
+                                                headers=_get_default_headers(token), json=payload, verify=False)
                         if response.status_code == 200:
-                            print(f"[bold green]Quality check id: {match.group(1)} updated successfully for datastore id: {datastore_id}[/bold green]")
+                            print(
+                                f"[bold green]Quality check id: {quality_check_id} updated successfully for datastore id: {datastore_id}[/bold green]")
                             total_updated_checks += 1
                         else:
-                            print(f"[bold red]Error updating quality check id: {match.group(1)} [/bold red]")
-                            log_error(f"Error updating quality check id: {match.group(1)} on datastore id: {datastore_id}. Details: {response.text}", BASE_PATH + "/errors.log")
-                    elif response.status_code == 200:
-                        print(f"[bold green]Quality check id: {response.json()['id']} for container: {quality_check['container']['name']} created successfully[/bold green]")
-                        total_created_checks += 1
+                            print(f"[bold red]Error updating quality check id: {quality_check_id} [/bold red]")
+                            log_error(
+                                f"Error updating quality check id: {quality_check_id} on datastore id: {datastore_id}. Details: {response.text}",
+                                BASE_PATH + "/errors.log")
+                    # If a quality check does not contain the description:
+                    # 1. We try to create quality check and verify for conflict
+                    #    a. If we notify a conflict, it will  update the check
+                    #    b. If there's no conflic, it will create a new one
                     else:
-                        log_error(f"Error creating quality check for datastore id: {datastore_id}. Details: {response.text}", BASE_PATH + "/errors.log")
-                            
+                        response = requests.post(base_url + f"quality-checks", headers=_get_default_headers(token),
+                                                 json=payload, verify=False)
+                        if response.status_code == 409:
+                            print(
+                                f"[bold yellow]Quality check for container: {quality_check['container']['name']} was already created on datastore id: {datastore_id}. Updating check...[/bold yellow]")
+                            match = re.search(r"id: (\d+)", response.text)
+                            response = requests.put(base_url + f"quality-checks/{match.group(1)}",
+                                                    headers=_get_default_headers(token), json=payload, verify=False)
+                            if response.status_code == 200:
+                                print(
+                                    f"[bold green]Quality check id: {match.group(1)} updated successfully for datastore id: {datastore_id}[/bold green]")
+                                total_updated_checks += 1
+                            else:
+                                print(f"[bold red]Error updating quality check id: {match.group(1)} [/bold red]")
+                                log_error(
+                                    f"Error updating quality check id: {match.group(1)} on datastore id: {datastore_id}. Details: {response.text}",
+                                    BASE_PATH + "/errors.log")
+                        elif response.status_code == 200:
+                            print(
+                                f"[bold green]Quality check id: {response.json()['id']} for container: {quality_check['container']['name']} created successfully[/bold green]")
+                            total_created_checks += 1
+                        else:
+                            log_error(
+                                f"Error creating quality check for datastore id: {datastore_id}. Details: {response.text}",
+                                BASE_PATH + "/errors.log")
+
         print(f"Updated a total of {total_updated_checks} quality checks.")
         print(f"Created a tottal of {total_created_checks} quality checks.")
         distinct_file_content(BASE_PATH + "/errors.log")
-      
+
 
 # Add the checks_app as a subcommand to the main app
 app.add_typer(checks_app, name="checks")
-
 
 if __name__ == "__main__":
     app()
