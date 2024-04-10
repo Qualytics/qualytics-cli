c1033ac7bee17a9b97190d0e3b126e8dc95b598e
Rodrigo Ogawa <rodrigo@qualytics.co>
2024-04-10 15:13:23 -0300

    Bump version: 0.1.15 → 0.1.16

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ project_urls = {
     "GitHub": "https://github.com/Qualytics/qualytics-cli",
     "Userguide": "https://qualytics.github.io/userguide/",
 }
-__version__ = "0.1.15"
+__version__ = "0.1.16"
 setup(
     name="qualytics-cli",
     packages=find_packages(),
